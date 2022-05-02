6d510e48d68c2ef4740f6420be78b6cb9ef75c3d
AlboMastro <alb.mastromonaco@gmail.com>
2022-05-02 16:06:12 +0200

    Exercise 3.7

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const morgan = require('morgan');
 const app = express();
 
+
 app.use(express.json());
+app.use(morgan("tiny"));
 
 let persons = [
   { 
@@ -87,7 +90,6 @@ app.post("/api/persons", (request, response) => {
 
   response.json(person);
 });
-
 
 const PORT = 3001
 app.listen(PORT, () => {
