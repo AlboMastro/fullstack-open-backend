04dfa66083a099f21195570493ccbb0fe2eee0de
AlboMastro <alb.mastromonaco@gmail.com>
2022-05-12 09:54:33 +0200

    Exercises 3.15 - 3.18

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ const express = require('express')
 const morgan = require('morgan');
 const cors = require('cors')
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  switch (error.name) {
+    case "CastError":
+      return response.status(400).send({ error: "malformatted id" });
+      break;
+    case "ParallelSaveError":
+      return response
+        .status(409)
+        .send({ error: "conflict error" });
+      break;
+
+    case "MongooseError":
+      return response
+        .status(500)
+        .send({ error: "there was an error" });
+    default:
+      return response
+      .status(500)
+      .send("Something went wrong");
+  }
+
+  next(error);
+};
+
 const app = express();
 app.use(express.json());
 app.use(cors())
@@ -11,25 +37,41 @@ app.use(express.static('build'))
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan('tiny'));
 
-const Person = require('./models/person')
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(person => {
-    response.json(person)
-  }) 
-})
+const Person = require('./models/person');
+const { model } = require('mongoose');
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((person) => {
+    response.json(person);
+    console.log(person.length)
+  });
+  
+});
+
+app.get("/api/info", (request, response) => {
+  Person.estimatedDocumentCount((err, count) => {
+    response.send(`
+    <p> Phonebook has info for ${count} people </p>
+      ${new Date()}
+    `);
+  });
+});
+
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+  .then((person) => {
     if (person) {
       response.json(person);
     } else {
       response.status(404).end();
     }
-  });
+  })
+  .catch(error => next(error));
 });
+app.use(errorHandler);
 
-app.post("/api/persons", morgan(':body'), (request, response) => {
+app.post("/api/persons", morgan(':body'), (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -49,6 +91,32 @@ app.post("/api/persons", morgan(':body'), (request, response) => {
     .then(savedPerson => {
     response.json(savedPerson)
   })
+
+  .catch(error => next(error))
+});
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    date: new Date(),
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 const PORT = process.env.PORT 
