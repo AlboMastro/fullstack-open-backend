32c546174df82a3d1e64010e8c034dadc4b4d943
Alberto Mastromonaco <alb.mastromonaco@gmail.com>
2022-07-21 17:19:50 +0200

    Exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -10,6 +10,19 @@ const dummy = (blogs) => {
     return dummyPlacer()
 }
 
+const totLikes = (blogs) => {
+    const getSumByKey = (arr, key) => {
+        return arr.reduce((accumulator, current) => accumulator + Number(current[key]), 0)
+    }
+    
+    if (blogs.length === 0) {
+        return 0
+    } else {
+        return getSumByKey(blogs, 'likes')
+    }
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totLikes
 }
